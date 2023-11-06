db8583690b0b16aafaf515e7effcccbdef525350
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-06 16:07:07 +0530

    StudRegNav component completed

--- src/Components/StudRegNav.jsx
+++ src/Components/StudRegNav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const StudRegNav = () => {
-  return (
-    <div>StudRegNav</div>
-  )
+    return (
+        <div>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Course App</a>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default StudRegNav
