f7715b89f802e2f2f8b02cef990e4de4ae3c86ab
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-06 18:56:57 +0530

    ClgSearchStud modified

--- src/Components/ClgSearchStud.jsx
+++ src/Components/ClgSearchStud.jsx
@@ -3,33 +3,39 @@ import React, { useState } from 'react'
 import ClgNavBar from './ClgNavBar'
 
 const ClgSearchStud = () => {
-    const [inputField,setInputField]=useState(
-        {clgName:sessionStorage.getItem("clgname"),studAdmNo:""}
+    const [inputField, setInputField] = useState(
+        { clgName: sessionStorage.getItem("clgname"), studAdmNo: "" }
     )
 
-    const [outputField,setOutputField]=useState(
-        [{clgName:"",studName:"",studAdmNo:"",studCurrSem:"",studDept:"",studPhNo:"",studEmailId:""}]
+    const [outputField, setOutputField] = useState(
+        [{ clgName: "", studName: "", studAdmNo: "", studCurrSem: "", studDept: "", studPhNo: "", studEmailId: "" }]
     )
 
-    const apiLink="http://localhost:3001/clgsearchstud"
+    const apiLink = "http://localhost:3001/clgsearchstud"
 
-    const inputHandler=(event)=>{
-        setInputField({...inputField,[event.target.name]:event.target.value})
+    const inputHandler = (event) => {
+        setInputField({ ...inputField, [event.target.name]: event.target.value })
     }
 
-    const readValue=()=>{
-        axios.post(apiLink,inputField).then(
-            (Response)=>{
-                setOutputField(Response.data)
-                console.log(Response.data)
-                setInputField({clgName:sessionStorage.getItem("clgname"),studAdmNo:""})
+    const readValue = () => {
+        axios.post(apiLink, inputField).then(
+            (Response) => {
+                if (Response.data.status == "success") {
+                    setOutputField(Response.data.data)
+                    console.log(Response.data.data)
+                    setInputField({studAdmNo:""})
+                } else {
+                    alert(Response.data.status)
+                    setInputField({studAdmNo:""})
+                    setOutputField( [{ studName: "", studAdmNo: "", studCurrSem: "", studDept: "", studPhNo: "", studEmailId: "" }])
+                }
             }
         )
     }
 
     return (
         <div>
-            <ClgNavBar/>
+            <ClgNavBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -45,10 +51,11 @@ const ClgSearchStud = () => {
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <button onClick={readValue} className="btn btn-warning">Search</button>
                             </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
                         </div>
                         <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <table className="table">
+                                <table className="table">
                                     <thead>
                                         <tr>
                                             <th scope="col">College Name</th>
@@ -61,19 +68,18 @@ const ClgSearchStud = () => {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {outputField.map(
-                                            (value,index)=>{
-                                                return <tr>
-                                                <th scope="row">{value.clgName}</th>
-                                                <td>{value.studName}</td>
-                                                <td>{value.studAdmNo}</td>
-                                                <td>{value.studCurrSem}</td>
-                                                <td>{value.studDept}</td>
-                                                <td>{value.studPhNo}</td>
-                                                <td>{value.studEmailId}</td>
+
+                                         <tr>
+                                                <th scope="row">{outputField.clgName}</th>
+                                                <td>{outputField.studName}</td>
+                                                <td>{outputField.studAdmNo}</td>
+                                                <td>{outputField.studCurrSem}</td>
+                                                <td>{outputField.studDept}</td>
+                                                <td>{outputField.studPhNo}</td>
+                                                <td>{outputField.studEmailId}</td>
                                             </tr>
-                                            }
-                                        )}
+                                           
+
                                     </tbody>
                                 </table>
                             </div>
