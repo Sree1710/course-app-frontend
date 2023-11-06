628e245d867844e4cdbf203aa084e44559b6d373
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-06 14:03:19 +0530

    AdminLogin component created

--- src/AdminLogin.jsx
+++ src/AdminLogin.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const AdminLogin = () => {
   return (
     <div>
-        
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
