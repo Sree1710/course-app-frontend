e77e99f8827c5479cc27e2ddcc26e0ab4eb374e1
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-06 14:02:15 +0530

    AdminNavBar component completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminAddClg from './AdminAddClg';
 import AdminViewClg from './AdminViewClg';
 import AdminViewStudent from './AdminViewStudent';
 import './App.css';
+import AdminLogin from './AdminLogin';
 
 function App() {
   return (
     <div>
-     <AdminViewStudent/>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' exact element={<AdminLogin/>}/>
+      <Route path='/admaddclg' exact element={<AdminAddClg/>}/>
+      <Route path='/admviewclg' exact element={<AdminViewClg/>}/>
+      <Route path='/admviewstud' exact element={<AdminViewStudent/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
